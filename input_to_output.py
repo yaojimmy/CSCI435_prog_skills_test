0de4c606602abff681b45305557c9d57a3e78291
Jimmy Yao <42041438+6PackOfRibs@users.noreply.github.com>
2022-09-05 20:22:32 -0400

    highlights leaf nodes with yellow rectangle

--- input_to_output.py
+++ input_to_output.py
@@ -1,6 +1,7 @@
-from lib2to3.pgen2.parse import ParseError
 from pathlib import Path
+import os
 import xml.etree.ElementTree as ET
+from PIL import Image, ImageDraw
 
 if __name__ == '__main__':
 
@@ -8,13 +9,23 @@ if __name__ == '__main__':
     for file in Path("Programming-Assignment-Data").iterdir():
 
         if file.suffix == '.xml':
-            print(file)
-            try:
-                myTree = ET.parse(file)
-                myRoot = myTree.getroot()
+            myTree = ET.parse(file)
+            myRoot = myTree.getroot()
 
+            with Image.open("Programming-Assignment-Data/" + os.path.splitext(file.name)[0] + ".png") as im:
+                draw = ImageDraw.Draw(im)
                 for node in myTree.iter('*'):
                     if len(node) == 0:
-                        print(node.attrib['text'])
-            except ParseError:
-                pass
+                        bounds = node.attrib['bounds']
+                        bounds = bounds[1:]
+                        bounds = bounds[:-1]
+
+                        bounds = bounds[0:bounds.index(']')] + ',' + bounds[bounds.index('[')+1:]
+                        bounds = bounds.split(',')
+                        bounds = list(map(int, bounds))
+                        w, h = bounds[2] - bounds[0], bounds[3] - bounds[1]
+                        bounds = [(bounds[0], bounds[1]), (bounds[2], bounds[3])]
+
+                        draw.rounded_rectangle(bounds, outline="yellow", width=3)
+            
+            im.save("Programming-Assignment-Output/" + os.path.splitext(file.name)[0] + ".png")
